88fc21b17f69a5d8a810d9a38957da47144d919b
Hamza <hamzazaidi253@gmail.com>
2025-09-22 16:39:59 +0200

    premier commit

--- index.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector(".operation");
-const operation = () =>{
-    const name = prompt("enter your name");
-    alert (`Your name is ${name}`);
-}
-btn.addEventListener('onclick' ,()=>(operation));
