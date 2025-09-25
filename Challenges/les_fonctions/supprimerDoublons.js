f2cd63197669c6459566174677ef3a3837c9bcaa
Hamza <hamzazaidi253@gmail.com>
2025-09-25 14:40:08 +0100

    supprimer double v02

--- Challenges/les_fonctions/supprimerDoublons.js
+++ Challenges/les_fonctions/supprimerDoublons.js
@@ -1,6 +1,31 @@
-function supprimerDoublons(tab){
-  return [...new Set(tab)]
+// i hade never pushed my brain to the limits untill this time
+let tab = ['hamza','zaidi','hamza','zaidi','ahmed','ahmed']
+
+function supprimerDoublons1(tab){
+    let repeted = [];
+    for(let i = 0 ; i < tab.length ; i++){
+        for(let j = i+1 ; j < tab.length ; j++){
+            if(tab[i] == tab[j]){
+                repeted.push(tab[i])
+            }
+        }
+    }
+    let indexs = [];
+    for(let i = 0 ; i < repeted.length ; i++){
+        indexs.push(tab.indexOf(repeted[i]))
+    }
+    for(let i = 0 ; i < indexs.length ; i++){
+        tab.splice(indexs[i],1);
+    }
+    return repeted;
 }
-let names = ['hamza','zaidi','hamza']
-console.log(supprimerDoublons(names));
+
+console.log(supprimerDoublons1(tab));
+
+
+
+
+
+
+
 
